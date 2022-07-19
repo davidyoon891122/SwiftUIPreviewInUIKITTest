9ff39c94620d63916a3e6f4173ed2b16e2026218
Yoonjiwon <k112751@koreainvestment.com>
2022-07-19 10:18:29 +0900

    Refactoring
    * Removed Useless codes
    * Removed UITests

--- PreviewTest/Scene/MyYellowButton.swift
+++ PreviewTest/Scene/MyYellowButton.swift
@@ -26,36 +26,10 @@ final class MyYellowButton: UIButton {
 
 #if canImport(SwiftUI) && DEBUG
 import SwiftUI
-struct UIViewPreview<View: UIView>: UIViewRepresentable {
-    let view: View
-
-    init(_ builder: @escaping () -> View) {
-        view = builder()
-    }
-
-    // MARK: - UIViewRepresentable
-
-    func makeUIView(context: Context) -> UIView {
-        return view
-    }
-
-    func updateUIView(_ view: UIView, context: Context) {
-        view.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-        view.setContentHuggingPriority(.defaultHigh, for: .vertical)
-    }
-}
-#endif
-
-#if canImport(SwiftUI) && DEBUG
-import SwiftUI
 
 struct MyYellowButtonPreview: PreviewProvider{
     static var previews: some View {
-        UIViewPreview {
-            let button = MyYellowButton(frame: .zero)
-            button.setTitle("buttonTest", for: .normal)
-            return button
-        }.previewLayout(.sizeThatFits)
+        MyYellowButton().showPreview()
     }
 }
 #endif
